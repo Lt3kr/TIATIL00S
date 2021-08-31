3ba36265bb4b5c5e6364e46bb6d38eb5fd71c710
Tolfx <matteus@tolfx.dev>
2021-08-31 15:51:35 +0200

    Small changes

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -8,11 +8,21 @@ namespace Basics
         {
             string[] test = new string[] { "xdddd", "D:" };
             int[] test2 = new int[] { 3,4,5,6 };
+            float[] test3 = new float[] { 3.1f,4.1f,5.1f,6.1f };
+            double[] test4 = new double[] { 3.1d, 4.1d, 5.1d, 6.1d };
+            bool[] test5 = new bool[] { false, true };
+
+            System.Int64 bigInt = 10000000000000000000;
 
             Loop(test);
             Loop(test2);
+            Loop(test3);
+            Loop(test4);
+            Loop(test5);
             
             Console.ReadKey();
+            var d = Console.ReadLine();
+            System.Console.WriteLine(d);
         }
 
         /// <summary>
@@ -49,6 +59,17 @@ namespace Basics
         /// Console logs whats in text
         /// </summary>
         static void Loop(float[] text)
+        {
+            foreach (var t in text)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+        /// <summary>
+        /// Console logs whats in text
+        /// </summary>
+        static void Loop(bool[] text)
         {
             foreach (var t in text)
             {
