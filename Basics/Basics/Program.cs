efebf2af288ebb0742c790d7dbb5ea01efcf24d6
Tolfx <matteus@tolfx.dev>
2021-09-07 16:02:47 +0200

    Modified < Program.cs >

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -6,23 +6,44 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            string[] test = new string[] { "xdddd", "D:" };
-            int[] test2 = new int[] { 3,4,5,6 };
-            float[] test3 = new float[] { 3.1f,4.1f,5.1f,6.1f };
-            double[] test4 = new double[] { 3.1d, 4.1d, 5.1d, 6.1d };
-            bool[] test5 = new bool[] { false, true };
-
-            Loop(test);
-            Loop(test2);
-            Loop(test3);
-            Loop(test4);
-            Loop(test5);
+            var number = (new findIndex()).run();
             
-            Console.ReadKey();
-            var d = Console.ReadLine();
-            System.Console.WriteLine(d);
-            System.Console.ReadLine();
-            System.Console.ReadLine();
+            System.Console.WriteLine((new int[] {1,2,3,4}));
+            foreach (var item in number)
+            {
+                System.Console.WriteLine(item);    
+            }
+            // string[] test = new string[] { "xdddd", "D:" };
+            // int[] test2 = new int[] { 21, 13, 51 };
+            // float[] test3 = new float[] { 3.1f,4.1f,5.1f,6.1f };
+            // double[] test4 = new double[] { 3.1d, 4.1d, 5.1d, 6.1d };
+            // bool[] test5 = new bool[] { false, true };
+            // Human human = new Human{ 
+            //     firstName = "Matteus",
+            //     lastName = "Westman",
+            //     isDead = true
+            // };
+
+            // var isHumanAlive = int.Parse(System.Console.ReadLine());
+
+            // // if()
+
+            // if(!human.isDead)
+            //     System.Console.WriteLine($"Human {human.fullName()} is alive!");
+            // else
+            //     System.Console.WriteLine($"Human {human.fullName()} is dead..");
+
+            // Loop(test);
+            // Loop(test2);
+            // Loop(test3);
+            // Loop(test4);
+            // Loop(test5);
+            
+            // Console.ReadKey();
+            // var d = Console.ReadLine();
+            // System.Console.WriteLine(d);
+            // System.Console.ReadLine();
+            // System.Console.ReadLine();
         }
 
         /// <summary>
